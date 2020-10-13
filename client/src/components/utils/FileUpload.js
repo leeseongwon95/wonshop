7f6d975b0ababa0ed6b00072e2e879f92748107c
leeseongwon95 <67531188+leeseongwon95@users.noreply.github.com>
2020-10-13 17:22:18 +0900

    Send image files to server

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,11 +1,30 @@
 import React from "react";
 import Dropzone from "react-dropzone";
 import { Icon } from "antd";
+import axios from "axios";
+import { response } from "express";
+import { replace } from "formik";
 
 function FileUpload() {
+  let formData = new FormData();
+
+  const config = {
+    header: { "content-type": "multipart/form-data" },
+  };
+  formData.append("file", files[0]);
+
+  const dropHandler = (files) => {
+    axios.post("/api/product/image", formData, config).then((response) => {
+      if (response.data.success) {
+      } else {
+        alert("파일을 저장하는데 실패하였습니다.");
+      }
+    });
+  };
+
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
-      <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+      <Dropzone onDrop={dropHandler}>
         {({ getRootProps, getInputProps }) => (
           <div
             style={{
---
+++ server/routes/product.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+//=================================
+//             Product
+//=================================
+
+router.post("/image", (req, res) => {
+  //가져온 이미지 저장
+});
+
+module.exports = router;
