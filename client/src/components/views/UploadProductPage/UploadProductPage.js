f66505d6c1977109c0c04e7fd73afbfd53398dd3
leeseongwon95 <67531188+leeseongwon95@users.noreply.github.com>
2020-10-13 16:18:46 +0900

    select option

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -4,6 +4,16 @@ import { useState } from "react";
 
 const { TextArea } = Input;
 
+const Continents = [
+  { key: 1, value: "Africa" },
+  { key: 2, value: "Europe" },
+  { key: 3, value: "Asia" },
+  { key: 4, value: "North America" },
+  { key: 5, value: "South America" },
+  { key: 6, value: "Australia" },
+  { key: 7, value: "Antarctica" },
+];
+
 function UploadProductPage() {
   const [Title, setTitle] = useState("");
   const [Description, setDescription] = useState("");
@@ -20,7 +30,9 @@ function UploadProductPage() {
   const priceChangeHandler = (event) => {
     setPrice(event.currentTarget.value);
   };
-
+  const continentChangeHandler = (event) => {
+    setContinent(event.currentTarget.value);
+  };
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
@@ -46,8 +58,12 @@ function UploadProductPage() {
         <Input type="number" onChange={priceChangeHandler} value={Price} />
         <br />
         <br />
-        <select>
-          <option></option>
+        <select onChange={continentChangeHandler}>
+          {Continents.map((item) => (
+            <option key={item.key} value={Continent}>
+              {item.value}
+            </option>
+          ))}
         </select>
         <br />
         <br />
