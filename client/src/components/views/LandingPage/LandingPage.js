24dbe0ac12279f730e06500470a32cedcfaa1259
leeseongwon95 <67531188+leeseongwon95@users.noreply.github.com>
2020-10-14 18:15:01 +0900

    Card and Image Slider

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,19 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
 import axios from "axios";
+import { Icon, Col, Card, Row } from "antd";
+import Meta from "antd/lib/card/Meta";
+import ImageSlider from "../../utils/ImageSlider";
 
 function LandingPage() {
+  const [Products, setProduct] = useState([]);
+
   useEffect(() => {
     axios.post("/api/product/products").then((response) => {
       if (response.data.success) {
-        console.log(response.data);
+        setProduct(response.data.productInfo);
       } else {
         alert("상품들을 가져오는데 실패했습니다.");
       }
     });
   }, []);
 
-  return <div>Landing Page</div>;
+  const renderCards = Products.map((product, index) => {
+    return (
+      <Col lg={6} md={8} xs={24} key={index}>
+        <Card cover={<ImageSlider images={product.images} />}>
+          <Meta title={product.title} description={`$${product.price}`} />
+        </Card>
+      </Col>
+    );
+  });
+
+  return (
+    <div style={{ width: "75%", margin: "3rem auto" }}>
+      <div style={{ textAlign: "center" }}>
+        <h2>
+          신상품! 지금 바로 확인하세요!
+          <Icon type="rocket" />
+        </h2>
+      </div>
+
+      {/* Filter */}
+
+      {/* Search */}
+
+      {/* Card */}
+
+      <Row gutter={[16, 16]}>{renderCards}</Row>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button>더보기</button>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
